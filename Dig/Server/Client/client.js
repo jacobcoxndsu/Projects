befd196a4397cf7efb39978c29b703ef737931a3
Ethan Brox <brox.ethan@gmail.com>
2017-11-11 12:03:28 -0600

    a few more

---
+++ Dig/Server/Client/client.js
@@ -0,0 +1,22 @@
+var socket = io();
+
+window.onload = function () {
+    //var socket = io();
+    var id = null;
+
+    var canvas = document.getElementById('canvas');
+    var c = canvas.getContext('2d');
+    width = canvas.width = window.innerWidth;
+    height = canvas.height = window.innerHeight;
+
+    /*socket.on('playerId', function (data) {
+        id = data.id;
+    });*/
+
+    var draw = function () {
+        window.requestAnimationFrame(draw);
+    }
+    
+    draw();
+    console.log("Game Loaded Successfuly");
+}
